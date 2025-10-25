2638659006ad95430e4c9392c6871afd6bc8519b
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2025-10-25 14:53:00 -0300

    update vercel

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import routes from "./routes/index.js";
+import routes from "./src/routes/index.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use("/", routes);
 
-app.listen(4000, () => console.log("Proxy Asaas rodando na porta 4000"));
+// só inicia o servidor se NÃO estiver em produção (Vercel cuida disso)
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+}
+
+export default app;
+
