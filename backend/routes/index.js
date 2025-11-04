9f9eb6c12c29e428a1441762e0139444d8406f14
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2025-11-04 13:58:09 -0300

    update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const asaasRoutes = require("./asaasRoutes");
 const webHookRoutes = require("./webHookRoutes");
 const timeRoutes = require("./timeRoutes");
+const googleSheetsRoutes = require("./googleSheetsRoutes");
 
 const router = express.Router();
 
 router.use("/asaas", asaasRoutes);
 router.use("/webhook", webHookRoutes);
 router.use("/time", timeRoutes);
+router.use("/google-sheets", googleSheetsRoutes);
 
 module.exports = router;
