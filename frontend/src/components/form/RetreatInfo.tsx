104f1a7e800be50c337a61b08ca51ac202c63370
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2026-01-11 19:18:40 -0300

    update

--- frontend/src/components/form/RetreatInfo.tsx
+++ frontend/src/components/form/RetreatInfo.tsx
@@ -23,7 +23,7 @@ export const RetreatInfo = () => {
         <strong>30/11/2025</strong>
         <br />até <strong>2x</strong> no cartão até{" "}
         <strong>31/12/2025</strong>
-        <br /><strong>1x</strong> no cartão até <strong>10/02/2026</strong>
+        <br /><strong>1x</strong> no cartão até <strong>01/02/2026</strong>
         <br />
         <br />
         <strong>DATAS DO RETIRO:</strong>
